7676d9f600d054d1d6c6525def6a8323b5c92ad6
dhopp1 <daniel.hopp1@gmail.com>
2023-12-08 11:36:28 +0100

    Making aggregate country df function robust to different aggregation and group_by columns

--- R/aggregate_country_data.r
+++ R/aggregate_country_data.r
@@ -1,48 +1,26 @@
-#' @import data.table dplyr
+#' @import data.table dplyr stringr
 #' @title Aggregate country data
 #' @name aggregate_country_data
 #' @description Combine all the years of data for a country in a particular HS classification. Purpose is to have a more robust distribution for 1->n conversions for \code{covert_hs} function, \code{map_df} parameter.
-#' @param data Dataframe with data for all years of a country in a given HS classification. Dataframe should be raw output from Comtrade with columns:
+#' @param data Dataframe with data for all years of a country in a given HS classification. Dataframe should be raw output from Comtrade
+#' @param agg_columns list of column names to be aggregated/summed up
+#' @param group_columns list of columns to group the aggregation by
 #' 
-#' \item{Year}{the year of the trade data}
-#' \item{FlowCode}{the HS version of the data}
-#' \item{ReporterCode}{code of the trade flow (export, import, etc.)}
-#' \item{ReporterLabel}{the name of the reporter country}
-#' \item{PartnerCode}{the code of the partner country}
-#' \item{CommodityCode}{the commodity code}
-#' \item{PartnerLabel}{the name of the partner country}
-#' \item{Value}{the value of recorded trade}
-#' 
-#' @return A \code{dataframe} containing the following same columns as the input dataframe, with \code{Year} set to 999 to denote it is an aggregation, but to make sure output df has the same columns as the input df.
+#' @return A \code{dataframe} containing the same columns as the input dataframe, less those not either aggregation columns or group by columns
 #'
 #' @export
-
-aggregate_country_data <- function (data) {
+aggregate_country_data <- function (data, agg_columns, group_columns) {
+  # only consider columns that are either to be aggregated or grouped by
+  columns <- colnames(data)[colnames(data) %in% c(agg_columns, group_columns)]
   output <- data %>% 
     data.table %>% 
-    mutate(Year = 999) # filler for Year column
+    select(all_of(columns))
   
-  output <- output[, `:=` (Value = sum(Value, na.rm=TRUE)), by = list(Year, FlowCode, ReporterCode, ReporterLabel, PartnerCode, PartnerLabel, CommodityCode)] %>% 
-    distinct() %>% 
-    data.frame()
-  
-  return (output)
-}
-
-# GTCDIT way
-aggregate_country_data <- function (data) {
-  output <- data %>% 
-    data.table
-  
-  output <- output[, `:=` (
-    CIFValue = sum(CIFValue, na.rm=TRUE), 
-    FOBValue = sum(FOBValue, na.rm=TRUE), 
-    Qty = sum(Qty, na.rm=TRUE)
-  ), 
-  by = list(CommodityCode, Origin, Transit, Mot, QtyUnitCode)] %>% 
+  # perform aggregation
+  agg_string <- str_interp("output[, `:=` (${paste0(unname(unlist(sapply(agg_columns, function (x) paste0(x, ' = sum(', x, ', na.rm=TRUE)')))), collapse = ', ')}), by = list(${paste0(group_columns, collapse = ',')})]")
+  output <- eval(parse(text = agg_string)) %>% 
     distinct() %>% 
     tibble()
-  output[output == FALSE] <- NA
   
   return (output)
 }
